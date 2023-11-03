3324836beef8a5d97669b20d34d68d7cf979deda
Umjiseung <eom071005123@gmail.com>
2023-11-03 23:42:46 +0900

    add::NotBlank Annotation

--- src/main/java/com/project/playlist/domain/playlist/data/dto/request/PlayListUpdateRequest.java
+++ src/main/java/com/project/playlist/domain/playlist/data/dto/request/PlayListUpdateRequest.java
@@ -4,15 +4,24 @@ import com.project.playlist.domain.playlist.data.entity.Category;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @RequiredArgsConstructor
 public class PlayListUpdateRequest {
     private final Long id;
+    @NotBlank
     private final String studentId;
+    @NotBlank
     private final String studentName;
+    @NotBlank
     private final String musicName;
+    @NotBlank
     private final String musicURL;
+    @NotBlank
     private final String musicContent;
+    @NotBlank
     private final Category category;
+
     private final String playListPW;
 }
--- src/main/java/com/project/playlist/domain/playlist/data/dto/request/PlayListWriteRequest.java
+++ src/main/java/com/project/playlist/domain/playlist/data/dto/request/PlayListWriteRequest.java
@@ -5,16 +5,25 @@ import com.project.playlist.domain.playlist.data.entity.Category;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @RequiredArgsConstructor
 public class PlayListWriteRequest {
     private final Long id;
+    @NotBlank
     private final String studentId;
+    @NotBlank
     private final String studentName;
+    @NotBlank
     private final String musicName;
+    @NotBlank
     private final String musicURL;
+    @NotBlank
     private final String musicContent;
+    @NotBlank
     private final Category category;
+    @NotBlank
     private final String playListPW;
 
 }
