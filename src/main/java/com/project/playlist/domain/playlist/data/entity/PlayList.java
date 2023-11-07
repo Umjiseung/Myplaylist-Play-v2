e120f747ac16feaaad5e25f6c9d354f350982580
Umjiseung <eom071005123@gmail.com>
2023-11-07 10:30:06 +0900

    update:: nullable = false

--- src/main/java/com/project/playlist/domain/playlist/data/entity/PlayList.java
+++ src/main/java/com/project/playlist/domain/playlist/data/entity/PlayList.java
@@ -16,14 +16,26 @@ public class PlayList {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String studentId;
+
+    @Column(nullable = false)
     private String studentName;
+
+    @Column(nullable = false)
     private String musicName;
+
+    @Column(nullable = false)
     private String musicURL;
+
+    @Column(nullable = false)
     private String musicContent;
 
     @Enumerated(EnumType.STRING)
     private Category category;
+
+    @Column(nullable = false)
     private String playListPW;
 
 }
