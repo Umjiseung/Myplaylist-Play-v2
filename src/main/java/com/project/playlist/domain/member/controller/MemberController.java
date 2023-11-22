e8d58c2bc424cb5f14c17d3f182aa43112e84932
Umjiseung <eom071005123@gmail.com>
2023-11-22 19:50:18 +0900

    add::MemberController

--- src/main/java/com/project/playlist/domain/member/controller/MemberController.java
+++ src/main/java/com/project/playlist/domain/member/controller/MemberController.java
@@ -1,4 +1,22 @@
 package com.project.playlist.domain.member.controller;
 
+import com.project.playlist.domain.member.data.dto.MemberResponseDto;
+import com.project.playlist.domain.member.service.MemberService;
+import com.project.playlist.global.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/member")
 public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/mypage")
+    public ResponseEntity<MemberResponseDto> findMemberInfoById() {
+        return ResponseEntity.ok(memberService.findMemberInfoById(SecurityUtil.getCurrentMemberId()));
+    }
 }
