0c86fe0310c5add291fc57175449110fe88b7a66
Umjiseung <eom071005123@gmail.com>
2023-11-22 20:13:25 +0900

    add::AuthService인터페이스 and 추상화

--- src/main/java/com/project/playlist/domain/auth/service/AuthService.java
+++ src/main/java/com/project/playlist/domain/auth/service/AuthService.java
@@ -1,6 +1,13 @@
 package com.project.playlist.domain.auth.service;
 
+import com.project.playlist.domain.auth.dto.TokenDto;
+import com.project.playlist.domain.auth.dto.TokenRequestDto;
+import com.project.playlist.domain.member.data.dto.MemberRequestDto;
+import com.project.playlist.domain.member.data.dto.MemberResponseDto;
+
 public interface AuthService {
 
-
+    MemberResponseDto signup(MemberRequestDto memberRequestDto);
+    TokenDto login(MemberRequestDto memberRequestDto);
+    TokenDto refresh(TokenRequestDto tokenRequestDto);
 }
