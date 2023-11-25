75e75f1065d7d34f716efab2f5ba45c9d3a4dfca
Umjiseung <eom071005123@gmail.com>
2023-11-25 23:30:48 +0900

    update::SecurityUtil Bean 등록

--- src/main/java/com/project/playlist/global/util/SecurityUtil.java
+++ src/main/java/com/project/playlist/global/util/SecurityUtil.java
@@ -4,15 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
 public class SecurityUtil {
 
     private SecurityUtil() { }
 
     // SecurityContext 에 유저 정보가 저장되는 시점
     // Request 가 들어올 때 JwtFilter 의 doFilter 에서 저장
-    @Bean
     public static Long getCurrentMemberId() {
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
