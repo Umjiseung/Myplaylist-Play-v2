168da420e35e237f06df09bd0d7fe9193fe7bd3e
Umjiseung <eom071005123@gmail.com>
2023-11-26 00:32:48 +0900

    add::MemberUtilsImpl

--- src/main/java/com/project/playlist/global/member/MemberUtils.java
+++ src/main/java/com/project/playlist/global/member/MemberUtils.java
@@ -1,30 +1,11 @@
 package com.project.playlist.global.member;
 
 import com.project.playlist.domain.member.data.entity.Member;
-import com.project.playlist.domain.member.repository.MemberRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
 
+public interface MemberUtils {
 
-@RequiredArgsConstructor
-public class MemberUtils {
-    private final MemberRepository memberRepository;
-
-    public Member getCurrentMember() {
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        return getMemberByEmail(email);
-    }
-
-    public Member getMemberByEmail(String email) {
-        return memberRepository.findByEmail(email).orElseThrow(RuntimeException::new);
-    }
-
-    public void checkExistName(String studentName) {
-        if (memberRepository.existsByStudentName(studentName));
-    }
-
-    public void checkExistEmail(String email) {
-        if(memberRepository.existsByEmail(email));
-    }
-
+    Member getCurrentMember();
+    Member getMemberByEmail(String email);
+    void checkExistName(String studentName);
+    void checkExistEmail(String email);
 }
---
+++ src/main/java/com/project/playlist/global/member/impl/MemberUtilsImpl.java
@@ -0,0 +1,30 @@
+package com.project.playlist.global.member.impl;
+
+import com.project.playlist.domain.member.data.entity.Member;
+import com.project.playlist.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+
+@RequiredArgsConstructor
+public class MemberUtilsImpl {
+    private final MemberRepository memberRepository;
+
+    public Member getCurrentMember() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return getMemberByEmail(email);
+    }
+
+    public Member getMemberByEmail(String email) {
+        return memberRepository.findByEmail(email).orElseThrow(RuntimeException::new);
+    }
+
+    public void checkExistName(String studentName) {
+        if (memberRepository.existsByStudentName(studentName));
+    }
+
+    public void checkExistEmail(String email) {
+        if(memberRepository.existsByEmail(email));
+    }
+
+}
