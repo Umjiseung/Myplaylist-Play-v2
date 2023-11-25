b81e130df6ecaf33c3f6d283bc05c717d7e235de
Umjiseung <eom071005123@gmail.com>
2023-11-25 23:53:22 +0900

    update::auth로 들어오는 요청 허용

--- src/main/java/com/project/playlist/global/config/SecurityConfig.java
+++ src/main/java/com/project/playlist/global/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.project.playlist.global.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -57,7 +58,7 @@ public class SecurityConfig {
                 // 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
                 .and()
                 .authorizeHttpRequests()
-                .requestMatchers("/auth/**").permitAll()
+                .antMatchers(HttpMethod.POST,"/auth/**").permitAll()
                 .anyRequest().authenticated()   // 나머지 API 는 전부 인증 필요
 
                 // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
