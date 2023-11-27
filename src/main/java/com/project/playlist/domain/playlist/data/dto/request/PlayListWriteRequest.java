3088fd4addb616a678c641467307ace31271c4e5
Umjiseung <eom071005123@gmail.com>
2023-11-27 14:48:40 +0900

    add::toEntity메서드 추가

--- src/main/java/com/project/playlist/domain/playlist/data/dto/request/PlayListWriteRequest.java
+++ src/main/java/com/project/playlist/domain/playlist/data/dto/request/PlayListWriteRequest.java
@@ -1,8 +1,11 @@
 package com.project.playlist.domain.playlist.data.dto.request;
 
 
+import com.project.playlist.domain.member.data.entity.Member;
 import com.project.playlist.domain.playlist.data.entity.Category;
 import javax.validation.constraints.NotBlank;
+
+import com.project.playlist.domain.playlist.data.entity.PlayList;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -26,5 +29,9 @@ public class PlayListWriteRequest {
     private Category category;
     @NotBlank
     private String playListPW;
+
+    public PlayList toEntity(Member member) {
+        return PlayList.builder().member(member).musicName(musicName).musicContent(musicContent).musicURL(musicURL).category(category).playListPW(playListPW).build();
+    }
 
 }
