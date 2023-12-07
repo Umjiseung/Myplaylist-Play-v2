e0c33a5ae90a55f6a91aaf9461fad3112893f057
Umjiseung <eom071005123@gmail.com>
2023-12-07 15:40:15 +0900

    config::RefreshToken Redis 연결하기

--- src/main/java/com/project/playlist/global/redis/RedisConfig.java
+++ src/main/java/com/project/playlist/global/redis/RedisConfig.java
@@ -1,6 +1,6 @@
 package com.project.playlist.global.redis;
 
-import io.lettuce.core.dynamic.annotation.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
