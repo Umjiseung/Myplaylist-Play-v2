e6371752af3a3394ae32a43dbe843b7fbd1dc926
Umjiseung <eom071005123@gmail.com>
2023-12-07 09:49:24 +0900

    update::중복된 코드 제거

--- src/main/java/com/project/playlist/domain/member/service/impl/MemberServiceImpl.java
+++ src/main/java/com/project/playlist/domain/member/service/impl/MemberServiceImpl.java
@@ -15,20 +15,18 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional(rollbackFor = {RuntimeException.class}, readOnly = true)
 public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
     private final PlayListUtils playListUtils;
     private final MemberUtils memberUtils;
 
-    @Transactional(rollbackFor = {RuntimeException.class})
     public MemberResponseDto myMemberInfo(String StudentId) {
         return memberRepository.findByStudentId(StudentId)
                 .map(MemberResponseDto::of)
                 .orElseThrow(() -> new RuntimeException("로그인 유저 정보가 없습니다."));
     }
 
-    @Transactional(rollbackFor = {RuntimeException.class})
     public List<MyPlaylistGetsResponse> getMyPlaylist() {
         Member member = memberUtils.getCurrentMember();
         return playListUtils.findPlaylistsByUserInfo(member);
