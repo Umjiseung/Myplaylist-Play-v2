db6b91edf488b28fd5e19f7fc0c8b031cbd932e5
Umjiseung <eom071005123@gmail.com>
2023-12-08 11:09:16 +0900

    add::RefreshToken Redis 적용

--- src/main/java/com/project/playlist/domain/member/data/entity/RefreshToken.java
+++ src/main/java/com/project/playlist/domain/member/data/entity/RefreshToken.java
@@ -1,26 +1,17 @@
 package com.project.playlist.domain.member.data.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+
+import org.springframework.data.redis.core.RedisHash;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@RedisHash(value = "refreshToken", timeToLive = 60)
 public class RefreshToken {
 
     @Id
-    @Column(nullable = false,name = "rt_key")
     private String key;
 
-    @Column(nullable = false,name = "rt_value")
     private String value;
 
     public void ExpireValue(String value) {
--- src/main/java/com/project/playlist/domain/member/repository/RefreshTokenRepository.java
+++ src/main/java/com/project/playlist/domain/member/repository/RefreshTokenRepository.java
@@ -2,9 +2,10 @@ package com.project.playlist.domain.member.repository;
 
 import com.project.playlist.domain.member.data.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken,Long> {
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken,String > {
     Optional<RefreshToken> findByKey(String key);
 }
