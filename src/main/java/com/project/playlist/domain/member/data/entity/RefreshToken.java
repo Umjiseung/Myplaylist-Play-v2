14e00da7b3d2bdef68fc44b0d9bde59b2f4f7275
Umjiseung <eom071005123@gmail.com>
2023-12-10 14:01:14 +0900

    deleted::어노테이션 정리 및 삭제

--- src/main/java/com/project/playlist/domain/member/data/entity/RefreshToken.java
+++ src/main/java/com/project/playlist/domain/member/data/entity/RefreshToken.java
@@ -1,22 +1,12 @@
 package com.project.playlist.domain.member.data.entity;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
 
-import javax.persistence.Entity;
 import javax.persistence.Id;
 
 
 @RedisHash(value = "refreshToken", timeToLive = 60)
-@Builder
-@Getter
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
 public class RefreshToken {
 
     @Id
