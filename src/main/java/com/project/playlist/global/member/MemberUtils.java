cb53161598caf6272a8e5cb78be07ccb138c945d
Umjiseung <eom071005123@gmail.com>
2023-12-25 00:01:59 +0900

    delete::불필요한 코드 리팩토링

--- src/main/java/com/project/playlist/global/member/MemberUtils.java
+++ src/main/java/com/project/playlist/global/member/MemberUtils.java
@@ -5,7 +5,4 @@ import com.project.playlist.domain.member.data.entity.Member;
 public interface MemberUtils {
 
     Member getCurrentMember();
-    Member getMemberByEmail(String email);
-    boolean checkExistName(String studentName);
-    boolean checkExistEmail(String email);
 }
--- src/main/java/com/project/playlist/global/member/impl/MemberUtilsImpl.java
+++ src/main/java/com/project/playlist/global/member/impl/MemberUtilsImpl.java
@@ -3,7 +3,6 @@ package com.project.playlist.global.member.impl;
 import com.project.playlist.domain.member.data.entity.Member;
 import com.project.playlist.domain.member.exception.MemberNotFoundException;
 import com.project.playlist.domain.member.repository.MemberRepository;
-import com.project.playlist.global.member.MemberDetailsImpl;
 import com.project.playlist.global.member.MemberUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,24 +18,9 @@ public class MemberUtilsImpl implements MemberUtils {
     @Override
     public Member getCurrentMember() {
         String id = SecurityContextHolder.getContext().getAuthentication().getName();
-        log.info(id);
         return memberRepository.findById(Long.valueOf(id))
                 .orElseThrow(MemberNotFoundException::new);
     }
 
-    @Override
-    public Member getMemberByEmail(String email) {
-        return memberRepository.findByEmail(email).orElseThrow(RuntimeException::new);
-    }
-
-    @Override
-    public boolean checkExistName(String studentName) {
-        return memberRepository.existsByStudentName(studentName);
-    }
-
-    @Override
-    public boolean checkExistEmail(String email) {
-        return memberRepository.existsByEmail(email);
-    }
 
 }
