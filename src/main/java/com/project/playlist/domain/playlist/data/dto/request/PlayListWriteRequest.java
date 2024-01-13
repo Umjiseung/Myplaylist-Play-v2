1f0eeef90277f01d61bb0b60f79c7a636304de42
Umjiseung <eom071005123@gmail.com>
2024-01-13 22:29:56 +0900

    refactor::자잘한 코드 오류 수정

--- src/main/java/com/project/playlist/domain/playlist/data/dto/request/PlayListWriteRequest.java
+++ src/main/java/com/project/playlist/domain/playlist/data/dto/request/PlayListWriteRequest.java
@@ -24,8 +24,6 @@ public class PlayListWriteRequest {
     private String musicContent;
     @NotNull
     private Category category;
-    @NotBlank
-    private String playListPW;
 
     public PlayList toEntity(Member member) {
         return PlayList.builder()
