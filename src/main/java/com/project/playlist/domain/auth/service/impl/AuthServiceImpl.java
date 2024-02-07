e7578340b604f155d1753cad7b6880f6f092ab67
Umjiseung <eom071005123@gmail.com>
2024-02-07 22:25:59 +0900

    update::login, refresh 및 logout 로직 변경

--- src/main/java/com/project/playlist/domain/auth/service/impl/AuthServiceImpl.java
+++ src/main/java/com/project/playlist/domain/auth/service/impl/AuthServiceImpl.java
@@ -2,19 +2,19 @@ package com.project.playlist.domain.auth.service.impl;
 
 import com.project.playlist.domain.auth.dto.*;
 import com.project.playlist.domain.auth.exception.AlreadyExistMemberException;
-import com.project.playlist.domain.auth.exception.InvalidTokenException;
+import com.project.playlist.domain.auth.exception.ExpiredTokenException;
+import com.project.playlist.domain.auth.exception.PasswordNotMatchException;
 import com.project.playlist.domain.auth.service.AuthService;
 import com.project.playlist.domain.member.data.dto.response.MemberResponse;
 import com.project.playlist.domain.member.data.entity.Member;
 import com.project.playlist.domain.member.data.entity.RefreshToken;
+import com.project.playlist.domain.member.exception.MemberNotFoundException;
 import com.project.playlist.domain.member.repository.MemberRepository;
 import com.project.playlist.domain.member.repository.RefreshTokenRepository;
 import com.project.playlist.global.member.MemberUtils;
 import com.project.playlist.global.security.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,15 +23,13 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(rollbackFor = {Exception.class})
 public class AuthServiceImpl implements AuthService {
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final MemberRepository memberRepository;
     private final RefreshTokenRepository refreshTokenRepository;
     private final PasswordEncoder passwordEncoder;
     private final TokenProvider tokenProvider;
-    private final MemberUtils memberUtils;
 
     public MemberResponse signup(SignUpRequest signUpRequest) {
-        if (memberRepository.existsByStudentId(signUpRequest.getStudentId())) {
+        if (memberRepository.existsByStudentIdAndEmail(signUpRequest.getStudentId(), signUpRequest.getEmail())) {
             throw new AlreadyExistMemberException();
         }
         Member member = signUpRequest.toMember(passwordEncoder);
@@ -39,48 +37,53 @@ public class AuthServiceImpl implements AuthService {
     }
 
     public TokenDto login(LoginRequest loginRequest) {
-        UsernamePasswordAuthenticationToken authenticationToken = loginRequest.toAuthentication();
+        Member member = memberRepository.findByEmail(loginRequest.getEmail())
+                .orElseThrow(MemberNotFoundException::new);
 
-        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        if (!passwordEncoder.matches(loginRequest.getPassword(), member.getPassword())) {
+            throw new PasswordNotMatchException();
+        }
 
-        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
+        TokenDto tokenDto = tokenProvider.generateTokenDto(member.getId(), member.getAuthority());
 
+        saveRefreshToken(tokenDto, member);
+
+        return tokenDto;
+    }
+
+    public TokenDto refresh(String refreshToken) {
+        String parseRefreshToken = tokenProvider.parseRefreshToken(refreshToken);
+
+        RefreshToken validRefreshToken = refreshTokenRepository.findById(parseRefreshToken)
+                .orElseThrow(ExpiredTokenException::new);
+
+        Member member = memberRepository.findById(validRefreshToken.getMemberId())
+                .orElseThrow(MemberNotFoundException::new);
+
+        TokenDto tokenDto = tokenProvider.generateTokenDto(member.getId(), member.getAuthority());
+
+        saveRefreshToken(tokenDto, member);
+        refreshTokenRepository.deleteById(validRefreshToken.getRefreshToken());
+
+        return tokenDto;
+    }
+
+    public void logout(String refreshToken) {
+        String parseRefreshToken = tokenProvider.parseRefreshToken(refreshToken);
+
+        RefreshToken validRefreshToken = refreshTokenRepository.findById(parseRefreshToken)
+                .orElseThrow(ExpiredTokenException::new);
+
+        refreshTokenRepository.deleteById(validRefreshToken.getRefreshToken());
+    }
+
+    private void saveRefreshToken(TokenDto tokenDto, Member member) {
         RefreshToken refreshToken = RefreshToken.builder()
-                .key(authentication.getName())
-                .value(tokenDto.getRefreshToken())
+                .refreshToken(tokenDto.getRefreshToken())
+                .memberId(member.getId())
+                .expiredAt(tokenDto.getRefreshTokenExpiresIn())
                 .build();
 
         refreshTokenRepository.save(refreshToken);
-
-        return tokenDto;
-    }
-
-    public TokenDto refresh(TokenRequestDto tokenRequestDto) {
-        if (!tokenProvider.validateToken(tokenRequestDto.getRefreshToken())) {
-            throw new InvalidTokenException();
-        }
-
-        Authentication authentication = tokenProvider.getAuthentication(tokenRequestDto.getAccessToken());
-
-        RefreshToken refreshToken = refreshTokenRepository.findById(authentication.getName())
-                .orElseThrow(()-> new NullPointerException("refreshToken is null"));
-
-
-        if (!refreshToken.getValue().equals(tokenRequestDto.getRefreshToken())) {
-            throw new InvalidTokenException();
-        }
-
-        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
-
-        RefreshToken newRefreshToken = refreshToken.updateValue(tokenDto.getRefreshToken());
-        refreshTokenRepository.save(newRefreshToken);
-
-
-        return tokenDto;
-    }
-
-    public void logout() {
-        Member member = memberUtils.getCurrentMember();
-        refreshTokenRepository.deleteById(member.getStudentName());
     }
 }
