d811500aeb9d4e518ca72372f2a111d30ba003b3
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-02-15 00:59:22 +0900

    add :: findById 메서드 추가

--- src/main/java/com/project/playlist/domain/member/repository/MemberRepository.java
+++ src/main/java/com/project/playlist/domain/member/repository/MemberRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member,Long> {
@@ -12,4 +13,5 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
 
     Optional<Member> findByStudentId(String studentId);
     boolean existsByStudentIdAndEmail(String studentId, String email);
+    Optional<Member> findById(UUID id);
 }
