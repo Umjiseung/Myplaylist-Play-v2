7d8d088d959fc7a30f4801940c66bf1601b4847f
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-02-16 16:03:47 +0900

    add :: getMyPlaylistCategory 로직 추가

--- src/main/java/com/project/playlist/domain/member/service/impl/MemberServiceImpl.java
+++ src/main/java/com/project/playlist/domain/member/service/impl/MemberServiceImpl.java
@@ -8,6 +8,10 @@ import com.project.playlist.domain.member.exception.MemberNotSameException;
 import com.project.playlist.domain.member.repository.MemberRepository;
 import com.project.playlist.domain.member.service.MemberService;
 import com.project.playlist.domain.playlist.data.dto.response.MyPlaylistGetsResponse;
+import com.project.playlist.domain.playlist.data.dto.response.PlayListGetsResponse;
+import com.project.playlist.domain.playlist.data.entity.Category;
+import com.project.playlist.domain.playlist.data.entity.PlayList;
+import com.project.playlist.domain.playlist.repository.PlayListRepository;
 import com.project.playlist.global.member.MemberUtils;
 import com.project.playlist.global.playlist.PlayListUtils;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,7 +29,7 @@ public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
     private final PlayListUtils playListUtils;
     private final MemberUtils memberUtils;
-    private final PasswordEncoder passwordEncoder;
+    private final PlayListRepository playListRepository;
 
     @Transactional(readOnly = true)
     public MemberResponse myMemberInfo(String studentId) {
@@ -45,19 +50,24 @@ public class MemberServiceImpl implements MemberService {
         return playListUtils.findPlaylistsByUserInfo(member);
     }
 
-    @Transactional
-    public void updatePassword(String studentId, UpdatePasswordRequest request) {
+    @Transactional(readOnly = true)
+    public List<MyPlaylistGetsResponse> getMyPlaylistCategory(Category category) {
         Member member = memberUtils.getCurrentMember();
-        Member findMember = memberRepository.findByStudentId(studentId)
-                .orElseThrow(MemberNotFoundException::new);
+        List<PlayList> myPlayLists = playListRepository.findPlayListByMemberAndCategory(member, category);
+        List<MyPlaylistGetsResponse> responses = new ArrayList<>();
 
-        if (member != findMember) {
-            throw new MemberNotSameException();
+        for (PlayList playList: myPlayLists) {
+            responses.add(new MyPlaylistGetsResponse(
+                    playList.getId(),
+                    playList.getMember(),
+                    playList.getMusicName(),
+                    playList.getMusicURL(),
+                    playList.getMusicContent(),
+                    playList.getCategory(),
+                    playList.getDate()
+            ));
         }
-
-        request.updatePassword(passwordEncoder, member.getPassword());
-
-        member.UpdateMember(request);
+        return responses;
     }
 
 }
