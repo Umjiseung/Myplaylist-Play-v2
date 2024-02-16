277f1d01a99b7d07ed7078fa69cbad9f3c3ca06b
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-02-16 23:43:58 +0900

    add :: CommentServiceImpl 추가

---
+++ src/main/java/com/project/playlist/domain/comment/service/impl/CommentServiceImpl.java
@@ -0,0 +1,66 @@
+package com.project.playlist.domain.comment.service.impl;
+
+import com.project.playlist.domain.comment.data.dto.request.UpdateCommentRequestDto;
+import com.project.playlist.domain.comment.data.dto.request.WriteCommentRequestDto;
+import com.project.playlist.domain.comment.data.entiry.Comment;
+import com.project.playlist.domain.comment.exception.CommentNotFoundException;
+import com.project.playlist.domain.comment.exception.CommentNotSameException;
+import com.project.playlist.domain.comment.repository.CommentRepository;
+import com.project.playlist.domain.comment.service.CommentService;
+import com.project.playlist.domain.member.data.entity.Member;
+import com.project.playlist.domain.member.repository.MemberRepository;
+import com.project.playlist.domain.playlist.data.entity.PlayList;
+import com.project.playlist.domain.playlist.repository.PlayListRepository;
+import com.project.playlist.global.member.MemberUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional(rollbackFor = {Exception.class})
+public class CommentServiceImpl implements CommentService {
+
+    private final CommentRepository commentRepository;
+    private final PlayListRepository playListRepository;
+    private final MemberUtils memberUtils;
+    private final MemberRepository memberRepository;
+
+    public void writeComment(UUID id, WriteCommentRequestDto request) {
+        Member member = memberUtils.getCurrentMember();
+
+        PlayList playList = playListRepository.findByIdOrIdNull(id);
+
+        Comment comment = request.toEntity(playList, member);
+        commentRepository.save(comment);
+    }
+
+    public void updateComment(UUID commentId, UpdateCommentRequestDto updateCommentRequestDto) {
+        Member member = memberUtils.getCurrentMember();
+
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(CommentNotFoundException::new);
+
+        if (member.getId() != comment.getMember().getId())
+            throw new CommentNotSameException();
+
+        commentRepository.save(updateCommentRequestDto.toEntity(comment));
+    }
+
+    public void deleteComment(UUID commentId) {
+        Member member = memberUtils.getCurrentMember();
+
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(CommentNotFoundException::new);
+
+        if (member.getId() != comment.getMember().getId())
+            throw new CommentNotSameException();
+
+        commentRepository.delete(comment);
+    }
+}
