eed6843af45bd4d45ba1ff77587cc13d25ccf5c5
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-02-17 00:05:30 +0900

    update :: MyPlaylistGetsResponse -> MyPlaylistGetsResponse 변경

--- src/main/java/com/project/playlist/global/playlist/PlayListUtils.java
+++ src/main/java/com/project/playlist/global/playlist/PlayListUtils.java
@@ -1,14 +1,14 @@
 package com.project.playlist.global.playlist;
 
 import com.project.playlist.domain.member.data.entity.Member;
-import com.project.playlist.domain.playlist.data.dto.response.MyPlaylistGetsResponse;
+import com.project.playlist.domain.playlist.data.dto.response.PlayListGetsResponse;
 import com.project.playlist.domain.playlist.data.entity.PlayList;
 
 import java.util.List;
 
 public interface PlayListUtils {
 
-    List<MyPlaylistGetsResponse> findPlaylistsByUserInfo(Member userInfo);
+    List<PlayListGetsResponse> findPlaylistsByUserInfo(Member userInfo);
 
     void validate(Member member, PlayList playList);
 }
