75e3fad160b123de6cc2718006064e07aa7a5945
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-02-16 23:45:52 +0900

    update :: ErrorCode 정리

--- src/main/java/com/project/playlist/global/error/exception/ErrorCode.java
+++ src/main/java/com/project/playlist/global/error/exception/ErrorCode.java
@@ -8,8 +8,15 @@ import lombok.Getter;
 @AllArgsConstructor
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ErrorCode {
+    // playlist
     PLAYLIST_NOT_FOUND(404, "Playlist Not Found"),
+
+    // member
     ALREADY_EXIST_MEMBER(400, "Already Exist Member"),
+    MEMBER_NOT_SAME(401,"Member Not Same"),
+    MEMBER_NOT_FOUND(404, "Member Not Found"),
+
+    // token
     NOT_THE_OWNER(400, "Not The Owner"),
     EXPIRED_TOKEN(401, "Expired Token"),
     INVALID_TOKEN(401, "Invalid Token"),
@@ -18,6 +25,9 @@ public enum ErrorCode {
     MEMBER_NOT_FOUND(404, "Member Not Found"),
     INVALID_TOKEN_TYPE(401,"Invalid Token Type"),
     PASSWORD_NOT_MATCH(400, "Password Not Match");
+
+    // auth
+    PASSWORD_NOT_MATCH(400, "Password Not Match"),
 
 
     private final int status;
