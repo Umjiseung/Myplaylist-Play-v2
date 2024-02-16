0ea49c239d853f55df6254523ef793b7aa1c0881
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-02-16 16:03:39 +0900

    add :: getMyPlaylistCategory 추가

--- src/main/java/com/project/playlist/domain/member/service/MemberService.java
+++ src/main/java/com/project/playlist/domain/member/service/MemberService.java
@@ -3,6 +3,7 @@ package com.project.playlist.domain.member.service;
 import com.project.playlist.domain.member.data.dto.request.UpdatePasswordRequest;
 import com.project.playlist.domain.member.data.dto.response.MemberResponse;
 import com.project.playlist.domain.playlist.data.dto.response.MyPlaylistGetsResponse;
+import com.project.playlist.domain.playlist.data.entity.Category;
 
 import java.util.List;
 
@@ -10,5 +11,5 @@ import java.util.List;
 public interface MemberService {
     MemberResponse myMemberInfo(String StudentId);
     List<MyPlaylistGetsResponse> getMyPlaylist();
-    void updatePassword(String studentId, UpdatePasswordRequest request);
+    List<MyPlaylistGetsResponse> getMyPlaylistCategory(Category category);
 }
