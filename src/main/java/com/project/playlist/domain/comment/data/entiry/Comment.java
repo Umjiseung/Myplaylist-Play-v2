7040ac14b933f9a2619469064a887a22a5b8e482
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-02-16 23:42:17 +0900

    add :: Comment Entity 추가

---
+++ src/main/java/com/project/playlist/domain/comment/data/entiry/Comment.java
@@ -0,0 +1,51 @@
+package com.project.playlist.domain.comment.data.entiry;
+
+import com.project.playlist.domain.member.data.entity.Member;
+import com.project.playlist.domain.playlist.data.entity.PlayList;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class Comment {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @Column(nullable = false)
+    private String comment;
+
+    @Column(name = "created_date")
+    @CreatedDate
+    private String createdDate;
+
+    @Column(name = "modified_date")
+    @LastModifiedDate
+    private String modifiedDate;
+
+    @ManyToOne
+    @JoinColumn(name = "playlist_id")
+    private PlayList playlist;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+}
