3e0644960492837fc5e146cc24775435c18a3920
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-02-16 23:42:53 +0900

    add :: Comment Controller 추가

---
+++ src/main/java/com/project/playlist/domain/comment/controller/CommentController.java
@@ -0,0 +1,39 @@
+package com.project.playlist.domain.comment.controller;
+
+import com.project.playlist.domain.comment.data.dto.request.UpdateCommentRequestDto;
+import com.project.playlist.domain.comment.data.dto.request.WriteCommentRequestDto;
+import com.project.playlist.domain.comment.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/comment")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/{id}")
+    public ResponseEntity<Void> writeComment(@PathVariable UUID id, @Valid @RequestBody WriteCommentRequestDto requestDto) {
+        commentService.writeComment(id, requestDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/{commentId}")
+    public ResponseEntity<Void> updateComment(@PathVariable UUID commentId, @Valid @RequestBody UpdateCommentRequestDto requestDto) {
+        commentService.updateComment(commentId, requestDto);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<Void> deleteComment(@PathVariable UUID commentId) {
+        commentService.deleteComment(commentId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+}
