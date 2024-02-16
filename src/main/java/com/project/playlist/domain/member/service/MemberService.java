7ed59db6dabdb09cb6d02d15aafe377ada1aaaf8
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-02-17 00:04:31 +0900

    update :: MyPlaylistGetsResponse -> MyPlaylistGetsResponse 변경

--- src/main/java/com/project/playlist/domain/member/service/MemberService.java
+++ src/main/java/com/project/playlist/domain/member/service/MemberService.java
@@ -1,8 +1,7 @@
 package com.project.playlist.domain.member.service;
 
-import com.project.playlist.domain.member.data.dto.request.UpdatePasswordRequest;
 import com.project.playlist.domain.member.data.dto.response.MemberResponse;
-import com.project.playlist.domain.playlist.data.dto.response.MyPlaylistGetsResponse;
+import com.project.playlist.domain.playlist.data.dto.response.PlayListGetsResponse;
 import com.project.playlist.domain.playlist.data.entity.Category;
 
 import java.util.List;
@@ -10,6 +9,6 @@ import java.util.List;
 
 public interface MemberService {
     MemberResponse myMemberInfo(String StudentId);
-    List<MyPlaylistGetsResponse> getMyPlaylist();
-    List<MyPlaylistGetsResponse> getMyPlaylistCategory(Category category);
+    List<PlayListGetsResponse> getMyPlaylist();
+    List<PlayListGetsResponse> getMyPlaylistCategory(Category category);
 }
