df330a444bb197217277fc53201eea735cadcac1
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-02-26 16:02:35 +0900

    update :: rollbackFor 속성 추가

--- src/main/java/com/project/playlist/global/member/CustomUserDetailsService.java
+++ src/main/java/com/project/playlist/global/member/CustomUserDetailsService.java
@@ -20,15 +20,13 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
-    @Override
-    @Transactional
+    @Transactional(rollbackFor = {Exception.class})
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return memberRepository.findByEmail(username)
                 .map(this::createUserDetails)
                 .orElseThrow(() -> new UsernameNotFoundException(username + " -> 데이터베이스에서 찾을 수 없습니다."));
     }
 
-    // DB 에 User 값이 존재한다면 UserDetails 객체로 만들어서 리턴
     private UserDetails createUserDetails(Member member) {
         GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.getAuthority().toString());
 
