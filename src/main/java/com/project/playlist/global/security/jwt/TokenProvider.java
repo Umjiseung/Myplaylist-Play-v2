c492e90a49b0c478cf60f08f0ed1904c325ed2fe
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-02-28 22:56:12 +0900

    update :: getAuthentication 메서드 로직 수정

--- src/main/java/com/project/playlist/global/security/jwt/TokenProvider.java
+++ src/main/java/com/project/playlist/global/security/jwt/TokenProvider.java
@@ -1,14 +1,14 @@
 package com.project.playlist.global.security.jwt;
 
 import com.project.playlist.domain.auth.presentation.dto.TokenDto;
-import com.project.playlist.domain.auth.exception.ExpiredTokenException;
 import com.project.playlist.domain.member.enums.Authority;
 import com.project.playlist.global.error.exception.ErrorCode;
 import com.project.playlist.global.error.exception.GlobalException;
-import com.project.playlist.global.security.exception.InvalidTokenTypeException;
+import com.project.playlist.global.security.auth.AuthDetailsService;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -17,7 +17,10 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import java.security.Key;
 import java.util.Arrays;
 import java.util.Collection;
@@ -25,7 +28,11 @@ import java.util.Date;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+import static com.project.playlist.global.security.filter.JwtFilter.AUTHORIZATION_HEADER;
+import static com.project.playlist.global.security.filter.JwtFilter.BEARER_PREFIX;
+
 @Component
+@RequiredArgsConstructor
 public class TokenProvider {
 
     private static final String AUTHORITIES_KEY = "auth";
@@ -33,12 +40,16 @@ public class TokenProvider {
     private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            // 30분
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
 
-    private final Key key;
+    @Value("${jwt.secret}")
+    private String secretKey;
+    private static Key key;
+    private final AuthDetailsService authDetailsService;
 
-    public TokenProvider(@Value("${jwt.secret}") String secretKey) {
+    @PostConstruct
+    public void init() {
 
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
-        this.key = Keys.hmacShaKeyFor(keyBytes);
+        key = Keys.hmacShaKeyFor(keyBytes);
     }
 
     public TokenDto generateTokenDto(UUID id, Authority authority) {
@@ -58,14 +69,9 @@ public class TokenProvider {
             throw new RuntimeException("권한 정보가 없는 토큰입니다.");
         }
 
-        Collection<? extends GrantedAuthority> authorities =
-            Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList());
+        UserDetails principal = authDetailsService.loadUserByUsername(parseClaims(accessToken).getSubject());
 
-        UserDetails principal = new User(claims.getSubject(), "", authorities);
-
-        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
+        return new UsernamePasswordAuthenticationToken(principal, "", principal.getAuthorities());
     }
 
     public String parseRefreshToken(String refreshToken) {
