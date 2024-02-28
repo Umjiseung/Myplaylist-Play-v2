4350f726ac3a15170497bca6ac6fe5b08f5ae7be
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-02-28 22:54:35 +0900

    add :: AuthDetails 추가

---
+++ src/main/java/com/project/playlist/global/security/auth/AuthDetails.java
@@ -0,0 +1,49 @@
+package com.project.playlist.global.security.auth;
+
+import com.project.playlist.domain.member.Member;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@AllArgsConstructor
+public class AuthDetails implements UserDetails {
+    private final Member member;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
