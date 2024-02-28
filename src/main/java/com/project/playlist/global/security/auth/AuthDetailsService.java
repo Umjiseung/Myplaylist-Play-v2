db192a29b7f2d303b12d6aa42214b192ebfd9c10
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-02-28 22:54:56 +0900

    add :: AuthDetailsService 추가

---
+++ src/main/java/com/project/playlist/global/security/auth/AuthDetailsService.java
@@ -0,0 +1,22 @@
+package com.project.playlist.global.security.auth;
+
+import com.project.playlist.domain.member.exception.MemberNotFoundException;
+import com.project.playlist.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(email)
+                .map(AuthDetails::new)
+                .orElseThrow(MemberNotFoundException::new);
+    }
+}
